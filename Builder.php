d98c0391ec90b46ed9cdb8c955e6a22c352d464a
Ahmed LAGGOUN <mqrkql@yahoo.fr>
2017-06-07 02:46:46 +0200

    Update Builder.php

--- Builder.php
+++ Builder.php
@@ -1,9 +1,8 @@
 <?php
 
-die();
 // BCVTC
 // By Ahmed LAGGOUN
-// Ahmed at reizon.net
+// Ahmed @ reizon.net
 
 class Api
 {
@@ -168,62 +167,3 @@ class Create extends BCVTC
         return $NewApiCall->EchoResponse();
     }
 }
-
-
-$Key = "YOUR_API_KEY";
-
-$data = [
-    'request'                   => "1",			
-    'client_civ'                => 'Mr',
-    'client_first_name'         => "Ahmed",			
-    'client_last_name'          => "Laggoun",			
-    'client_email'              => 'mail@site.fr',
-    'client_phone'              => "0601020304",			
-    'client_comment'            => "Bonjour",			
-    'paxes'                     => '1',
-    'pickupaddress'             => "37 Rue du Rocher, 75008 Paris",			
-    'dropoffaddress'            => "50 avenue daumesnil 75012 paris",			
-    'triptype'                  => '10',
-    'carid'                     => "3",			
-    'distancetext'              => "20 kms",			
-    'durationtext'              => '20 minutes',
-    'price_ht'                  => 20.00,			
-    'price_ttc'                 => 20.20,			
-    'pay_methode'               => 'es',
-    'pickupdatetime'            => '2017-06-03 20:20:20',
-    'driverid'                  => '2'
-    ];
-
-$CreateTicket = new Create($Key);
-
-$CreateTicket->CreateTicket($data);
-        
-
-
-
-
-
-
-
-
-//$TicketId = "eyIJX2rmhbL7";
-//
-//$Ticket = new Ticket($Key);
-//
-//echo $Ticket->GetTicket($TicketId);
-
-
-
-//$Request = new RequestPrice($Key);
-//
-//$Request->PickUpAddress("37 Rue du Rocher, 75008 Paris");
-//
-//$Request->DropOffAddress("37 Rue du Rocher, 75008 Paris");
-//
-//$Request->TripType("10");
-//
-//$Request->BuildData();
-//
-//$Request->Send();
-
-?>
